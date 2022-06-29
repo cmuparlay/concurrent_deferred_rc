090332a6dca86badc96635ed10d67623fa7de160
Daniel Anderson <dlanders@cs.cmu.edu>
2022-06-29 12:36:52 -0400

    Change memory orders in test_queue to all seq_cst

--- tests/test_queue.cpp
+++ tests/test_queue.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-static const int N = utils::num_threads() - 1;   // Number of threads
+static const size_t N = utils::num_threads() - 1;   // Number of threads
 static const int M = 10000;                      // Number of operations per thread
 
 
@@ -61,16 +61,16 @@ void test_par() {
   puts("PARALLEL PRODUCER AND CONSUMER TEST...");
 
   cdrc::weak_ptr_queue::atomic_queue<int> q;
-  std::atomic<int> done;
+  std::atomic<size_t> done;
   std::vector<long long int> consumer_sums(N/2);
   std::vector<long long int> producer_sums(N/2);
 
   std::vector<thread> consumers;
   consumers.reserve(N/2);
-  for (int i = 0; i < N/2; i++) {
+  for (size_t i = 0; i < N/2; i++) {
     consumers.emplace_back([i, &q, &done, &consumer_sums] {
       long long int local_sum = 0;
-      while (done.load(std::memory_order_acquire) < N/2 || q.peek()) {
+      while (done.load() < N/2 || q.peek()) {
         auto val = q.dequeue();
         if(val) local_sum += val.value();
       }
@@ -80,7 +80,7 @@ void test_par() {
 
   std::vector<thread> producers;
   producers.reserve(N/2);
-  for (int i = 0; i < N/2; i++) {
+  for (size_t i = 0; i < N/2; i++) {
     producers.emplace_back([i, &q, &done, &producer_sums] {
       long long int local_sum = 0;
       for(int j = 0; j < M; j++) {
@@ -88,15 +88,15 @@ void test_par() {
         local_sum += j;
       }
       producer_sums[i] = local_sum;
-      done.fetch_add(1, std::memory_order_acq_rel);
+      done.fetch_add(1);
     });
   }
 
-  while (done.load(std::memory_order_acquire) < N/2) {}
+  while (done.load() < N/2) {}
 
   long long actualsum = 0;
   long long checksum = 0;
-  for (int i = 0; i < N/2; i++) {
+  for (size_t i = 0; i < N/2; i++) {
     producers[i].join();
     consumers[i].join();
     actualsum += producer_sums[i];
@@ -113,7 +113,7 @@ void test_par2() {
 
   // The queue contains one element per thread
   cdrc::weak_ptr_queue::atomic_queue<int> q;
-  for (int t = 0; t < N; t++) {
+  for (size_t t = 0; t < N; t++) {
     q.enqueue(t);
   }
 
@@ -121,7 +121,7 @@ void test_par2() {
   std::vector<std::thread> threads;
   threads.reserve(N);
 
-  for (int t = 0; t < N; t++) {
+  for (size_t t = 0; t < N; t++) {
     threads.emplace_back([&q, &sums, t]() {
       long long int local_sum = 0;
       for (size_t i = 0; i < M; i++) {
