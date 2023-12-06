37bb2081bacd0b1ca6f68e38c2ef5e7a825ce315
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2023-12-05 19:37:53 -0500

    Another attempt at reproducing the bug

--- test/test_bug.cpp
+++ test/test_bug.cpp
@@ -1,21 +1,32 @@
 #include "gtest/gtest.h"
 
-#include <cdrc/marked_arc_ptr.h>
+#include <cdrc/atomic_rc_ptr.h>
+#include <cdrc/atomic_weak_ptr.h>
+
+#include <thread>
 
 using namespace cdrc;
 using namespace std;
 
+void task1(cdrc::rc_ptr<int> &x) {
+  sleep(0);
+  x = nullptr;
+}
+
+void task2(cdrc::weak_ptr<int> y) {
+  cdrc::rc_ptr<int> z = y.lock();
+}
+
 TEST(TestBug, Idk) {
-  thread first_thread([&]() {
+  const size_t num_trials = 10000;
+  for (size_t trial = 0; trial < num_trials; ++trial) {
+    cdrc::rc_ptr<int> x = cdrc::make_rc<int>(5);
+    cdrc::weak_ptr<int> y = x;
 
-  });
+    jthread other_thread(task2, move(y));
 
-  thread second_thread([&]() {
-
-  });
-
-  first_thread.join();
-  second_thread.join();
+    task1(x);
+  }
 
   ASSERT_TRUE(true);
 }
