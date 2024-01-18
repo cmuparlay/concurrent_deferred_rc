e6876f4bcf86c620d73e7dd0c7bca249b218edf7
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2024-01-17 20:16:15 -0500

    autopep8

--- benchmarks/memory_reclamation/run_experiments.py
+++ benchmarks/memory_reclamation/run_experiments.py
@@ -48,8 +48,6 @@
 # Test Mode 27 : ObjRetire:u50rq50:range=200M:prefill=100M
 
 
-
-
 import multiprocessing
 import os
 import sys
@@ -60,178 +58,193 @@ import create_graphs as graph
 
 datastructures = ['hashtable', 'list', 'bst']
 
+
 def tostring(ds, wl):
-  return 'exp-'+ds+'-'+wl
+    return 'exp-'+ds+'-'+wl
 
 # Returns True if the given command returns a non-zero return code,
 # i.e., is probably a valid command
+
+
 def valid_command(cmd):
-  DEVNULL = open(os.devnull, 'r+b', 0)
-  returncode = subprocess.call([cmd], stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL, shell=True)
-  return returncode == 0
+    DEVNULL = open(os.devnull, 'r+b', 0)
+    returncode = subprocess.call(
+        [cmd], stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL, shell=True)
+    return returncode == 0
 
-def convert(seconds): 
-  seconds = seconds % (24 * 3600) 
-  hour = seconds // 3600
-  seconds %= 3600
-  minutes = seconds // 60
-  seconds %= 60
-  return f'{hour}h:{minutes:02}m:{seconds:02}s'
 
-def run(ds, wl, tr, count, outfile): 
-  num_runs = 0;
-  for th in threads:
-    num_threads = max(32, th+1)
-    cmd = "NUM_THREADS=" + str(num_threads) + " " + preamble + binary + " -i " + str(runtime) + " -m " + str(wl) + " -r " + str(ds) + " -t " + str(th) + " -v "
-    if tr != "":
-      cmd += "-d tracker=" + tr
-    if not count:
-      print(cmd)
-    for i in range(repeats):
-      if count:
-        num_runs += 1
-      else:
-        os.system(cmd + " >> " + outdir+outfile)
-  return num_runs
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return f'{hour}h:{minutes:02}m:{seconds:02}s'
+
+
+def run(ds, wl, tr, count, outfile):
+    num_runs = 0
+    for th in threads:
+        num_threads = max(32, th+1)
+        cmd = "NUM_THREADS=" + str(num_threads) + " " + preamble + binary + " -i " + str(
+            runtime) + " -m " + str(wl) + " -r " + str(ds) + " -t " + str(th) + " -v "
+        if tr != "":
+            cmd += "-d tracker=" + tr
+        if not count:
+            print(cmd)
+        for i in range(repeats):
+            if count:
+                num_runs += 1
+            else:
+                os.system(cmd + " >> " + outdir+outfile)
+    return num_runs
+
 
 if __name__ == "__main__":
-  sizes = ['100', '1000', '100K', '1M', '10M', '100M']
-  update_percents = [1, 10, 50]
+    sizes = ['100', '1000', '100K', '1M', '10M', '100M']
+    update_percents = [1, 10, 50]
 
-  parser = argparse.ArgumentParser(description='concurrent deffered reference counting benchmarks')
+    parser = argparse.ArgumentParser(
+        description='concurrent deffered reference counting benchmarks')
 
-  parser.add_argument('--datastructure', '-d', type=str, default='all', help=f'datastructure to benchmark {[*datastructures, "all"]}')
-  parser.add_argument('--size', '-s', type=str, default='100', help=f'??? {sizes}')
-  parser.add_argument('--update_percent', '-u', type=int, default=10, help=f'??? {update_percents}')
+    parser.add_argument('--datastructure', '-d', type=str, default='all',
+                        help=f'datastructure to benchmark {[*datastructures, "all"]}')
+    parser.add_argument('--size', '-s', type=str,
+                        default='100', help=f'??? {sizes}')
+    parser.add_argument('--update_percent', '-u', type=int,
+                        default=10, help=f'??? {update_percents}')
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  smr_datastructure = {'hashtable': 1,
-                      'list': 7,
-                      'bst': 13}
-  rc_datastructures = {'hashtable' : [3, 4, 5, 6],
-                      'list' : [9,10,11,12],
-                      'bst' : [15,16,17,18],}
-  wl_num =          {
-                    '100-50':1,
-                    '1000-50':2,
-                    '100K-50':3,
-                    '1M-50':4,
-                    '10M-50':5,
-                    '100-10':6,
-                    '1000-10':7,
-                    '100K-10':8,
-                    '1M-10':9,
-                    '10M-10':10,
-                    '100-1':11,
-                    '1000-1':12,
-                    '100K-1':13,
-                    '1M-1':14,
-                    '10M-1':15,
-                    '100M-50':19,
-                    '100M-10':20,
-                    '100M-1':21,
-                    '100K-50rq':24,
-                    '1M-50rq':25,
-                    '10M-50rq':26,
-                    '100M-50rq':27,
-                    }
-  experiments = [("list", '1000-10'),
-              ("hashtable", '100K-10'),
-              ("bst", '100K-10'),
-              ("bst", '100M-10'),
-              ("bst", '100K-1'),
-              ("bst", '100K-50'),]
+    smr_datastructure = {'hashtable': 1,
+                         'list': 7,
+                         'bst': 13}
+    rc_datastructures = {'hashtable': [3, 4, 5, 6],
+                         'list': [9, 10, 11, 12],
+                         'bst': [15, 16, 17, 18], }
+    wl_num = {
+        '100-50': 1,
+        '1000-50': 2,
+        '100K-50': 3,
+        '1M-50': 4,
+        '10M-50': 5,
+        '100-10': 6,
+        '1000-10': 7,
+        '100K-10': 8,
+        '1M-10': 9,
+        '10M-10': 10,
+        '100-1': 11,
+        '1000-1': 12,
+        '100K-1': 13,
+        '1M-1': 14,
+        '10M-1': 15,
+        '100M-50': 19,
+        '100M-10': 20,
+        '100M-1': 21,
+        '100K-50rq': 24,
+        '1M-50rq': 25,
+        '10M-50rq': 26,
+        '100M-50rq': 27,
+    }
+    experiments = [("list", '1000-10'),
+                   ("hashtable", '100K-10'),
+                   ("bst", '100K-10'),
+                   ("bst", '100M-10'),
+                   ("bst", '100K-1'),
+                   ("bst", '100K-50'),]
 
-  graphs_only = False
-  test_mode = False
+    graphs_only = False
+    test_mode = False
 
-  if len(sys.argv) == 3 and sys.argv[2] == "test":
-    runtime = 1
-    repeats = 1
-    test_mode = True
-  else:
-    runtime = 5 # Change this variable to change runtime (measured in seconds)
-    repeats = 3 # Change this variable to chage number of iterations
+    if len(sys.argv) == 3 and sys.argv[2] == "test":
+        runtime = 1
+        repeats = 1
+        test_mode = True
+    else:
+        # Change this variable to change runtime (measured in seconds)
+        runtime = 5
+        repeats = 3  # Change this variable to chage number of iterations
 
-  # Compute thread counts to use for experiments
-  P = multiprocessing.cpu_count()
-  if not test_mode:
-    p_step_size = P // 5
-    threads = [1] + [k*p_step_size for k in range(1,5)] + [P] + [P + k*p_step_size for k in range(1,3)]
-  else:
-    threads = [1, P // 2, P, P + P // 2]
+    # Compute thread counts to use for experiments
+    P = multiprocessing.cpu_count()
+    if not test_mode:
+        p_step_size = P // 5
+        threads = [1] + [k*p_step_size for k in range(1, 5)] + [P] + [
+            P + k*p_step_size for k in range(1, 3)]
+    else:
+        threads = [1, P // 2, P, P + P // 2]
 
+    trackers = ["HazardOpt -d emptyf=2", "Hyaline -d emptyf=2",
+                "RCU -d epochf=10 -d emptyf=2", "Range_new -d epochf=40 -d emptyf=2"]
 
-  trackers = ["HazardOpt -d emptyf=2", "Hyaline -d emptyf=2", "RCU -d epochf=10 -d emptyf=2", "Range_new -d epochf=40 -d emptyf=2"]
+    exp_to_run = []
+    if args.datastructure == 'all':
+        for exp in experiments:
+            exp_to_run.append(exp)
+    else:
+        ds = args.datastructure
+        size = args.size
+        up = args.update_percent
+        if 'rq' in str(up):
+            trackers_copy = []
+            for tr in trackers:
+                if 'HazardOpt' not in tr:
+                    trackers_copy.append(tr)
+            trackers = trackers_copy
+        wl = f'{size}-{up}'
+        if wl not in wl_num:
+            print(f'invalid workload: must be one of {[*wl_num.keys()]}')
+            print(f'you entered: {wl} (size-update_percent)')
+            exit(1)
+        exp_to_run.append((ds, wl))
 
-  exp_to_run = []
-  if args.datastructure == 'all':
-    for exp in experiments:
-      exp_to_run.append(exp)
-  else:
-    ds = args.datastructure
-    size = args.size
-    up = args.update_percent
-    if 'rq' in str(up):
-      trackers_copy = []
-      for tr in trackers:
-        if 'HazardOpt' not in tr:
-          trackers_copy.append(tr)
-      trackers = trackers_copy
-    wl = f'{size}-{up}'
-    if wl not in wl_num:
-      print(f'invalid workload: must be one of {[*wl_num.keys()]}')
-      exit(1)
-    exp_to_run.append((ds, wl))
+    binary = "./bin/release/main"
+    preamble = ""
 
+    if valid_command('jemalloc-config --libdir'):
+        preamble += "LD_PRELOAD=`jemalloc-config --libdir`/libjemalloc.so.`jemalloc-config --revision` "
+    else:
+        print('Note: Not dynamically linking jemalloc because jemalloc could not be found')
+    if valid_command('numactl -i all echo'):
+        preamble += " numactl -i all "
+    else:
+        print('Note: Not running with NUMA since numactl is not installed or is not valid on this system')
 
-  binary = "./bin/release/main"
-  preamble = ""
+    num_experiments = 0
+    estimated_time = 0
 
-  if valid_command('jemalloc-config --libdir'):
-      preamble += "LD_PRELOAD=`jemalloc-config --libdir`/libjemalloc.so.`jemalloc-config --revision` "
-  else:
-      print('Note: Not dynamically linking jemalloc because jemalloc could not be found')
-  if valid_command('numactl -i all echo'):
-      preamble += " numactl -i all "
-  else:
-      print('Note: Not running with NUMA since numactl is not installed or is not valid on this system')
+    outdir = 'results/'
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
 
-  num_experiments = 0
-  estimated_time = 0
+    progress = 0
 
-  outdir = 'results/'
-  if not os.path.exists(outdir):
-    os.mkdir(outdir)
+    if not graphs_only:
+        for count in (1, 0):
+            if count == 0:
+                print(
+                    f'Running {num_experiments} experiments in {convert(num_experiments*runtime)}')
+                print(f'\tthread counts: {", ".join(map(str, threads))}')
+                print(f'\truntime: {runtime}')
+                print(f'\trepeats: {repeats}')
+            for exp in exp_to_run:
+                ds = exp[0]
+                wl = exp[1]
+                outfile = f'{tostring(ds, wl)}.out'
+                os.system(f' > {outdir}{outfile}')
+                for rc_ds in rc_datastructures[ds]:
+                    num_experiments += run(rc_ds,
+                                           wl_num[wl], "", count, outfile)
+                    if count == 0:
+                        progress += len(threads)*repeats
+                        print(str(progress) + " out of " + str(num_experiments))
+                for tr in trackers:
+                    num_experiments += run(
+                        smr_datastructure[ds], wl_num[wl], tr, count, outfile)
+                    if count == 0:
+                        progress += len(threads)*repeats
+                        print(str(progress) + " out of " + str(num_experiments))
 
-
-  progress = 0
-
-  if not graphs_only:
-    for count in (1, 0):
-      if count == 0:
-        print(f'Running {num_experiments} experiments in {convert(num_experiments*runtime)}')
-        print(f'\tthread counts: {", ".join(map(str, threads))}')
-        print(f'\truntime: {runtime}')
-        print(f'\trepeats: {repeats}')
-      for exp in exp_to_run:
-        ds = exp[0]
-        wl = exp[1]
-        outfile = f'{tostring(ds, wl)}.out'
-        os.system(f' > {outdir}{outfile}')
-        for rc_ds in rc_datastructures[ds]:
-          num_experiments += run(rc_ds, wl_num[wl], "", count, outfile)
-          if count == 0:
-            progress += len(threads)*repeats
-            print(str(progress) + " out of " + str(num_experiments))
-        for tr in trackers:
-          num_experiments += run(smr_datastructure[ds], wl_num[wl], tr, count, outfile)
-          if count == 0:
-            progress += len(threads)*repeats
-            print(str(progress) + " out of " + str(num_experiments))
-
-
-  # create graphs
-  for exp in exp_to_run:
-    graph.graph_results_from_file(tostring(exp[0],exp[1]), '')
+    # create graphs
+    for exp in exp_to_run:
+        graph.graph_results_from_file(tostring(exp[0], exp[1]), '')
